150add44be18e6afddad5811da082600397deadc
kijavnash <kjinash@gmail.com>
2020-10-12 17:34:48 -0400

    HELP ME!

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ def main():
 
         win.fill(black)
 
-        textsurface = myfont.render("THIS IS NORMAL.", False, white)
+        textsurface = myfont.render("""GAME OVER.""", False, white)
         win.blit(textsurface, (0, 0))  # change the coordinates to put it in a different place
 
         pygame.display.update()
