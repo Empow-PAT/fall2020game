afbcba1fa368c95460546857307465b57600386f
kijavnash <kjinash@gmail.com>
2020-10-26 16:18:19 -0400

    Menu Code.

--- run.py
+++ run.py
@@ -6,15 +6,32 @@ from fall2020game.players import *
 def main():
     pygame.init()
 
-    windowwidth = 400
-    windowheight = 400
+    windowwidth = 800
+    windowheight = 800
+
+
+
     win = pygame.display.set_mode((windowwidth, windowheight))
     pygame.display.set_caption("Starter")
     black = (0, 0, 0)
     white = (255, 255, 255)
 
-    myfont = pygame.font.SysFont('Impact', 30)  # change the 30 for a different text size
+    myfont = pygame.font.SysFont('Impact', 100)  # change the 30 for a different text size
 
+    class PlayButton:
+        def __init__(self):
+            self.Color = (150, 240, 30)
+            self.x = 200
+            self.y = 400
+            self.width = 400
+            self.height = 100
+            self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        def tick(self, win):
+            self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+            pygame.draw.rect(win, self.Color, self.rect)
+    playbutton = PlayButton()
 
     run = True
     while run:
@@ -24,16 +41,18 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        keys = pygame.key.get_pressed()
-
         win.fill(black)
+        playbutton.tick(win)
 
-        textsurface = myfont.render("""GAME OVER.""", False, white)
-        win.blit(textsurface, (0, 0))  # change the coordinates to put it in a different place
+        textsurface = myfont.render(""" PLAY """, False, white)
+        win.blit(textsurface, (300, 390))  # change the coordinates to put it in a different place
 
         pygame.display.update()
 
 
 if __name__ == '__main__':
     main()
+
+
+
 
