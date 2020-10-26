ed6d4c46317dda6e5d3325487484d314b82bc23f
Luckstern <Luckstern@users.noreply.github.com>
2020-10-26 18:11:42 -0400

    Added extra month code and fixed get request url

--- apis/start_screen.py
+++ apis/start_screen.py
@@ -1,18 +1,21 @@
 import requests
 from random import *
 from datetime import date
-import json
 
 date = date.today()
 split_date = str(date).split("-")
 Year = randint(2015, int(split_date[0]))
 Month = randint(1, int(split_date[1]))
+if str(Year) == split_date[0]:
+    Month = randint(1, int(split_date[1]))
+else:
+    Month = randint(1, 12)
 if str(Month) == split_date[1]:
     Day = randint(1, int(split_date[2]))
 else:
     Day = randint(1, 30)
 
-req = requests.get("https://api.nasa.gov/planetary/apod?api_key=Guv4egEEeC4kQxHBTEu4DDH26HGW4cWLNCYo2Npd&date={}-{}-{}".format(randint(2015, int(split_date[0])), randint(1, int(split_date[1])), randint(1, int(split_date[2]))))
+req = requests.get(f"https://api.nasa.gov/planetary/apod?api_key=Guv4egEEeC4kQxHBTEu4DDH26HGW4cWLNCYo2Npd&date={Year}-{Month}-{Day}")
 
 apod_json = req.json()
 
