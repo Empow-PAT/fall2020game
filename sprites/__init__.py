823b87cdc9163ef83b48e31b151e3cf357b5f479
Patrick Greene <patrick.anton.greene@gmail.com>
2020-11-02 16:30:33 -0500

    Add doc string to load_sprites function.

--- sprites/__init__.py
+++ sprites/__init__.py
@@ -5,7 +5,9 @@ from os.path import dirname, abspath, join
 
 HERE = dirname(abspath(__file__))
 
+
 def load_sprites():
+    """Load the image sprites from this folder, for easy access."""
     sprites = {}
     for file_name in listdir(HERE):
         if file_name.startswith('sprite_') and file_name.endswith('.png'):
@@ -14,6 +16,7 @@ def load_sprites():
             file_full_path = join(HERE, file_name)
             sprites[name_wo_png] = pygame.image.load(file_full_path) 
     return sprites
+
 
 sprites = load_sprites()
 
