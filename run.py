2668aabdd57ae8d58b7fb8cf9e02b99cfa393871
pagreene <patrick.anton.greene@gmail.com>
2020-11-16 17:49:44 -0500

    Change run2 to run.py.

--- run.py
+++ run.py
@@ -1,11 +1,33 @@
 import sys
 import pygame
 from fall2020game.players import *
+import pygame_menu
+nickname = "Javi"
 
 
-def main():
-    global run
+def Nickname(value):
+    global nickname
+
+    nickname = value
+
+
+def main(gameStart=None):
+    global nickname
     pygame.init()
+    nickname = "Nickname"
+    black = pygame_menu.baseimage.BaseImage(
+        image_path="Images/Loading Page.png",
+        drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL,
+    )
+    playbutton2 = pygame_menu.baseimage.BaseImage(
+        image_path="Images/Play Button.png",
+        drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL,
+    )
+    ships2 = pygame_menu.baseimage.BaseImage(
+        image_path="Images/Ships.png",
+        drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL,
+    )
+    X = "Images/X Button.png"
 
     windowwidth = 800
     windowheight = 800
@@ -17,21 +39,21 @@ def main():
 
     myfont = pygame.font.SysFont('Impact', 100)  # change the 30 for a different text size
     annihilator = Annihilator()
+    mytheme = pygame_menu.themes.Theme(
+        menubar_close_button=False,
+        title_font=pygame_menu.font.FONT_MUNRO,
+        title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_NONE,
+        background_color=black,
+        widget_selection_effect=pygame_menu.widgets.NoneSelection(),
+    )
+    menu = pygame_menu.Menu(windowheight-1, windowwidth-1, " ", theme=mytheme)
 
-    class PlayButton:
-        def __init__(self):
-            self.Color = (150, 240, 30)
-            self.x = 200
-            self.y = 400
-            self.width = 400
-            self.height = 100
-            self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+    menu.add_text_input('', default='ENTER NICKNAME HERE', onchange=Nickname, cursor_selection_enable=True, selection_effect= pygame_menu.widgets.HighlightSelection())
+    playbutton = menu.add_button("        ", gameStart, background_color=playbutton2, font_size=72)
+    ships = menu.add_button("         ", gameStart, background_color=ships2, font_size=36)
+    pygame.display.update()
 
-        def tick(self, win):
-            self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-
-            pygame.draw.rect(win, self.Color, self.rect)
-    playbutton = PlayButton()
+    menu.mainloop(win)
 
     run = True
     while run:
@@ -58,7 +80,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
