972aaed60a78db2fc3a6ae57fba988de8ab6ee56
kijavnash <kjinash@gmail.com>
2020-11-16 16:28:03 -0500

    Menu Page

--- Run Imrpoved.py
+++ Run Imrpoved.py
@@ -14,18 +14,33 @@ def main(gameStart=None):
     pygame.init()
     nickname = "Nickname"
     black = (0, 0, 0)
-
+    playbutton2 = pygame_menu.baseimage.BaseImage(
+        image_path="Images/Play Button.png",
+    drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL,
+    )
+    ships2 = pygame_menu.baseimage.BaseImage(
+        image_path="Images/Ships.png",
+    drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL,
+    )
+    X = ("Images/X Button.png")
 
     windowwidth = 800
     windowheight = 800
     win = pygame.display.set_mode((windowwidth, windowheight))
     win = pygame.display.set_mode((windowwidth, windowheight))
-    win.fill(black)
+    mytheme = pygame_menu.themes.Theme(
+        menubar_close_button=False,
+        title_font=pygame_menu.font.FONT_MUNRO,
+        title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_UNDERLINE_TITLE,
+        background_color=black,
+        widget_selection_effect=pygame_menu.widgets.NoneSelection(),
+    )
+    menu = pygame_menu.Menu(windowheight-1, windowwidth-1, " ", theme= mytheme)
 
-    menu = pygame_menu.Menu(windowheight-1, windowwidth-1, 'Astral Run',theme=pygame_menu.themes.THEME_DARK)
+
     menu.add_text_input('Player 1 : ', default='Nickname', onchange=Nickname)
-    menu.add_button('Play', gameStart)
-    menu.add_button('Quit', pygame_menu.events.EXIT)
+    playbutton = menu.add_button("        ", gameStart, background_color=playbutton2, font_size=72)
+    ships = menu.add_button("         ", gameStart, background_color=ships2, font_size=36)
     pygame.display.update()
 
     menu.mainloop(win)
